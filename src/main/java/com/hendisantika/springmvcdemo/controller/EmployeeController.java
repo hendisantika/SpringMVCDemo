6b517a9a93432fcd79040be0a3f3a9eddce183f1
Hendi Santika <hendisantika@gmail.com>
2020-01-01 06:34:11 +0700

    #16 - Add editEmployee API in EmployeeController

--- src/main/java/com/hendisantika/springmvcdemo/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springmvcdemo/controller/EmployeeController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +38,20 @@ public class EmployeeController {
         }
         return new ModelAndView("redirect:/employees");
     }
+
+    @RequestMapping(value = "/edit/{id}")
+    public ModelAndView editEmployee(@ModelAttribute("employee") Employee employee, @PathVariable("id") int id) {
+        ModelAndView model = new ModelAndView("employees");
+
+        employee = employeeDAO.getEmployeeById(id);
+        List<Employee> employeeList = employeeDAO.getAllEmployees();
+
+        model.addObject("employee", employee);
+        model.addObject("employeeList", employeeList);
+
+        return model;
+
+    }
+
 
 }
