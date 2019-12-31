9e3405c548c0362a4e5dbe2e3c09275ff1482ed1
Hendi Santika <hendisantika@gmail.com>
2020-01-01 06:30:44 +0700

    #9 - Add saveEmployee in EmployeeDAOImpl

--- src/main/java/com/hendisantika/springmvcdemo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/hendisantika/springmvcdemo/dao/EmployeeDAOImpl.java
@@ -1,5 +1,8 @@
 package com.hendisantika.springmvcdemo.dao;
 
+import com.hendisantika.springmvcdemo.entity.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +18,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeDAOImpl.class);
     private JdbcTemplate jdbcTemplate;
+
+    // JdbcTemplate setter
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Saving a new Employee
+    public void saveEmployee(Employee employee) {
+        String sql = "insert into Employee values(?,?,?,?)";
+        LOGGER.info("DAO Called {}", sql);
+        jdbcTemplate.update(sql, employee.getId(), employee.getName(), employee.getDept(), employee.getAge());
+    }
 }
